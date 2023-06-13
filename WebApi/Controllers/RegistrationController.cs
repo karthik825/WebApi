8450b38b5fbe5290b4047fad94d72b7aa91bdc97
karthik825 <karthik.k@cspl.com>
2023-06-13 15:08:35 +0530

    ASP.NET CONNECTED TO DATABASE USING C#

---
+++ WebApi/Controllers/RegistrationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {   
+        private readonly IConfiguration _configuration;
+
+        public RegistrationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpPost]
+        [Route("registration")]
+
+        public string registration(Registration registration)
+        {
+            SqlConnection con = new SqlConnection(_configuration.GetConnectionString("ToysCon").ToString());
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(UserName,Password,Email,IsActive) VALUES('"+registration.UserName +"', '"+registration.Password +"', '"+registration.Email +"', '" +registration.IsActive+"')", con);
+            int i=cmd.ExecuteNonQuery();
+            if(i>0)
+            {
+                return "command executed";
+            }
+            else
+            {
+                return "error";
+            }
+        }
+    }
+}
---
+++ WebApi/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.Models
+{
+    public class Response
+    {
+        public   int statusCode { get; set; }
+        public string statusMessage { get; set; }
+        
+    }
+}
